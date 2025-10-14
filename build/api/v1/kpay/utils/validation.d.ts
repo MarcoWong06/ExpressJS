6000c9829651cc7cd4c833fc08f11a48e1aa40fb
Charles Ting <charles.ting@cityray.com>
2025-10-14 10:34:46 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    build/api/index.d.ts
    	deleted:    build/api/index.js
    	deleted:    build/api/index.js.map
    	deleted:    build/api/v1/kpay/config/constants.d.ts
    	deleted:    build/api/v1/kpay/config/constants.js
    	deleted:    build/api/v1/kpay/config/constants.js.map
    	deleted:    build/api/v1/kpay/index.d.ts
    	deleted:    build/api/v1/kpay/index.js
    	deleted:    build/api/v1/kpay/index.js.map
    	deleted:    build/api/v1/kpay/routes/AllHostedCheckoutOrder.d.ts
    	deleted:    build/api/v1/kpay/routes/AllHostedCheckoutOrder.js
    	deleted:    build/api/v1/kpay/routes/AllHostedCheckoutOrder.js.map
    	deleted:    build/api/v1/kpay/services/kpayService.d.ts
    	deleted:    build/api/v1/kpay/services/kpayService.js
    	deleted:    build/api/v1/kpay/services/kpayService.js.map
    	deleted:    build/api/v1/kpay/types/allHostedCheckoutOrder.d.ts
    	deleted:    build/api/v1/kpay/types/allHostedCheckoutOrder.js
    	deleted:    build/api/v1/kpay/types/allHostedCheckoutOrder.js.map
    	deleted:    build/api/v1/kpay/types/index.d.ts
    	deleted:    build/api/v1/kpay/types/index.js
    	deleted:    build/api/v1/kpay/types/index.js.map
    	deleted:    build/api/v1/kpay/types/kpayApi.d.ts
    	deleted:    build/api/v1/kpay/types/kpayApi.js
    	deleted:    build/api/v1/kpay/types/kpayApi.js.map
    	deleted:    build/api/v1/kpay/utils/crypto.d.ts
    	deleted:    build/api/v1/kpay/utils/crypto.js
    	deleted:    build/api/v1/kpay/utils/crypto.js.map
    	deleted:    build/api/v1/kpay/utils/validation.d.ts
    	deleted:    build/api/v1/kpay/utils/validation.js
    	deleted:    build/api/v1/kpay/utils/validation.js.map
    	deleted:    build/server-index.d.ts
    	deleted:    build/server-index.js
    	deleted:    build/server-index.js.map
    	deleted:    build/test-index.d.ts
    	deleted:    build/test-index.js
    	deleted:    build/test-index.js.map

--- build/api/v1/kpay/utils/validation.d.ts
+++
@@ -1,5 +0,0 @@
-import type { OrderRequest } from "../types";
-export declare class ValidationError extends Error {
-    constructor(message: string);
-}
-export declare const validateOrderRequest: (body: Partial<OrderRequest>) => void;
